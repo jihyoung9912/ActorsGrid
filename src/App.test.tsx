d71ff61bdf028c945370b3f509ecac82ed21883b
김지형 <jihyoung9912@gmail.com>
2023-08-18 00:58:03 -0700

    Initial Setting of Jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from 'App';
 
-test('renders learn react link', () => {
+test('Shows the main page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const TestElement = screen.getByText('Test');
+  expect(TestElement).toBeInTheDocument();
 });
