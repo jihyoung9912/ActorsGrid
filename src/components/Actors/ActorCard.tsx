53fd6e7a48bca504a1d30cf59bb217e2ea9965c9
김지형 <jihyoung9912@gmail.com>
2023-08-22 00:34:36 -0700

    feat: Implement Infinity scroll

--- src/components/Actors/ActorCard.tsx
+++ src/components/Actors/ActorCard.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Flex } from 'components/common';
 import { fetchPopularActorsList } from 'apis/apis';
+import throttle from 'utils/throttle';
 
 interface IActorData {
   profile_path: string;
@@ -10,24 +11,27 @@ interface IActorData {
 
 const ActorCardContainer = styled(Flex)`
   width: 13rem;
-  height: 300px;
-  background-color: #f0f0f3;
+  height: 20rem;
+  margin: 1rem;
   box-shadow:
     -10px -10px 15px 0px #f6f6f6,
     10px 10px 15px 0px #cecece;
   border-radius: 13px;
+  background-color: #f0f0f3;
   //box-shadow:
   //  inset -7px -7px 5px 0px #f6f6f6,
   //  inset 7px 7px 5px 0px #cecece;
-  transition: 0.5s all ease-in-out;
-  margin: 1rem;
+  transition: 0.2s all ease-in-out;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.05);
+  }
 `;
 
 const ActorImg = styled.img`
   width: 100%;
   height: 80%;
   border-radius: 13px;
-  background-color: black;
 `;
 
 const ActorNameContainer = styled(Flex)`
@@ -37,30 +41,50 @@ const ActorNameContainer = styled(Flex)`
   height: 20%;
 `;
 
-const ActorCard: React.FC = () => {
+const ActorCard = () => {
   const [actorsData, setActorsData] = useState<IActorData[]>([]);
+  const [pageNumber, setPageNumber] = useState(1);
 
-  useEffect(() => {
-    if (!actorsData.length) {
-      fetchPopularActorsData();
-    }
-  }, [actorsData]);
-
-  const fetchPopularActorsData = async () => {
+  /*
+   * @params pageNumber
+   * Fetch Popular Actors data with page number
+   */
+  const fetchPopularActorsData = async (page: number) => {
     try {
-      const response = await fetchPopularActorsList(1); // Fetch actors data using your fetchPopularActorsList function
-      setActorsData(response.results);
+      const response = await fetchPopularActorsList(page);
+      setActorsData((prevActorsData) => [...prevActorsData, ...response.results]);
     } catch (error) {
       console.error('Error fetching actor data:', error);
     }
   };
 
-  console.log(actorsData);
+  // Data Fetching when pageNumber changed
+  useEffect(() => {
+    fetchPopularActorsData(pageNumber);
+  }, [pageNumber]);
+
+  // Scrolling Events Handlers with Throttling
+  const handleScroll = throttle(() => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) {
+      // Increase pageNumber for next page
+      fetchPopularActorsData(pageNumber + 1);
+      // Update pageNumber state
+      setPageNumber((prevPageNumber) => prevPageNumber + 1);
+    }
+  }, 500);
+
+  // Manage scrolling Events
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll]);
 
   return (
     <Flex $wrap="wrap" $justify="flex-start">
       {actorsData.map((actor, index) => (
-        <ActorCardContainer key={index} $wrap="wrap">
+        <ActorCardContainer key={actor.name + index} $wrap="wrap">
           <ActorImg
             src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`}
             alt={actor.name}
