898f022e05694eb8e1abe922ddc13f3f2e4171df
김지형 <jihyoung9912@gmail.com>
2023-08-23 03:33:07 -0700

    chore: apis Actor Data Promise type

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,8 +1,9 @@
 import fetchApi from 'utils/fetchAPI';
 import { popularActorsUrl, searchActorUrl } from 'constants/URLS';
+import { IActorData } from '../types/IActors';
 
-type TFetchPopularActorsList = (pageNumber: number) => Promise<any>;
-type TFetchActorsListWithKeyword = (pageNumber: number, name: string) => Promise<any>;
+type TFetchPopularActorsList = (pageNumber: number) => Promise<IActorData>;
+type TFetchActorsListWithKeyword = (pageNumber: number, name: string) => Promise<IActorData>;
 
 export const fetchPopularActorsList: TFetchPopularActorsList = async (pageNumber) => {
   const url = `${popularActorsUrl}?&language=en&page=${pageNumber}`;
