99a2141e80977e932342a9f20531d14f8ea3aa5c
김지형 <jihyoung9912@gmail.com>
2023-08-23 05:04:51 -0700

    style: Error Component desing

--- src/components/common/Error/ErrorFallback.tsx
+++ src/components/common/Error/ErrorFallback.tsx
@@ -1,4 +1,16 @@
+import styled from 'styled-components';
+import { FlexWrapper } from 'components/common';
+
+const ErrorContainer = styled(FlexWrapper)`
+  width: 100%;
+  height: 100vh;
+`;
+
 const ErrorFallback = () => {
-  return <div>There's an Error !!</div>;
+  return (
+    <ErrorContainer $justify="center">
+      <img src={`${process.env.PUBLIC_URL}/error.png`} alt="Error Image" />
+    </ErrorContainer>
+  );
 };
 export default ErrorFallback;
